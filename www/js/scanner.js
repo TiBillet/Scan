fdc36d1bf559bd714a6d6b17f08681fa010cea08
Pierre Comte <p.s.comte@hotmail.fr>
2025-01-26 19:05:05 +0100

    lien directe

--- www/js/scanner.js
+++ www/js/scanner.js
@@ -17,18 +17,32 @@ function scanQRCode() {
             console.error(err);
           }
         } else {
-          document.getElementById("qr-content").innerText = "Contenu : " + text;
+          // mettre à jour le contenu scanné
+          const qrContent = document.getElementById("qr-content");
+          qrContent.innerText = text;
+
+          // rendre l'URL cliquable
+          qrContent.onclick = function () {
+            openInAppBrowser(text);
+          };
           console.log("QR Code scanné :", text);
         }
 
-        QRScanner.destroy(); // Arrêter la caméra
+        QRScanner.destroy(); // arreter la cam
       });
 
-      QRScanner.show(); // Afficher la caméra
+      QRScanner.show(); // afficher la cam
     } else if (status.denied) {
       alert("Permission refusée. Activez-la dans les paramètres.");
     } else {
       alert("Permission non accordée. Essayez à nouveau.");
     }
   });
+}
+
+// fonction pour ouvrir l'url dans InAppBrowser
+function openInAppBrowser(url) {
+  cordova.InAppBrowser.open(url, "_system");
+  // "_system" pour ouvrir dans le navigateur par défaut
+  //"_blank" pour l'ouvrir dans fenetre de l'app
 }
